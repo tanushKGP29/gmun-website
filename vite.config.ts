baca6207395941441e42c5fd8bb0979d6564e2be
Tanush <tanush.24@kgpian.iitkgp.ac.in>
2025-07-11 20:20:39 +0530

    gmun-3 commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gmun-webpage/",
-
+  
   server: {
     host: "::",
     port: 8080,
